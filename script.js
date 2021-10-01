c982da7cfa52298bad8a0882b36beff423abec60
gabrielles6281 <91750595+gabrielles6281@users.noreply.github.com>
2021-10-01 12:53:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Dice"; // update the part BETWEEN the "quotes"
+let userName = "gabrielles6281"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
